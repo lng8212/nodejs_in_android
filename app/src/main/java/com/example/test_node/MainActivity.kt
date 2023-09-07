ff189b84f3ca89b26cf8a280dc6c64d8e26382bd
LongKD <longkd@ikameglobal.com>
2023-09-07 09:11:15 +0700

    test send object

--- app/src/main/java/com/example/test_node/MainActivity.kt
+++ app/src/main/java/com/example/test_node/MainActivity.kt
@@ -7,22 +7,33 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.test_node.databinding.ActivityMainBinding
 import org.json.JSONObject
 import org.liquidplayer.service.MicroService
+import org.liquidplayer.service.MicroService.EventListener
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     val TAG = "TAG"
+
+    private lateinit var readyListener: EventListener
+    private lateinit var pongListener: EventListener
+
+    private lateinit var service: MicroService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getIP()
-        val readyListener = MicroService.EventListener { service, _, _ ->
-            val payload = JSONObject()
-            payload.put("hallo", "die Weld")
-            service.emit("ping",payload)
+        setUpClick()
+        handleNode()
+    }
+
+    private fun handleNode() {
+        readyListener = EventListener { service, _, _ ->
+//            val payload = JSONObject()
+//            payload.put("hallo", "die Weld")
+//            service.emit("ping", payload)
         }
-        val pongListener = MicroService.EventListener { _, _, jsonObject ->
+        pongListener = EventListener { _, _, jsonObject ->
             val message = jsonObject.getString("message")
             runOnUiThread {
 //                binding.txtMain.text = message
@@ -34,16 +45,52 @@ class MainActivity : AppCompatActivity() {
                 service.addEventListener("pong", pongListener)
             }
         val uri = MicroService.Bundle(this, "example")
-        val service = MicroService(
+        service = MicroService(
             this, uri,
             startListener
         )
         service.start()
     }
 
-    private fun getIP(){
+    private fun getIP() {
         val wifiManager = getSystemService(WIFI_SERVICE) as WifiManager
         val ipAddress: String = Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
         binding.txtIp.text = ipAddress
+    }
+
+    private fun setUpClick() {
+        binding.run {
+            btnPair.setOnClickListener {
+                val payload = JSONObject()
+                payload.put("pair", "pair")
+                service.emit("pingPair", payload)
+            }
+            btnConnect.setOnClickListener {
+                val payload = JSONObject()
+                payload.put("connect", "connect")
+                service.emit("pingConnect", payload)
+            }
+            btnVolPlus.setOnClickListener {
+                val payload = JSONObject()
+                payload.put("volPlus", "volPlus")
+                service.emit("pingVolPlus", payload)
+            }
+            btnVolMinus.setOnClickListener {
+                val payload = JSONObject()
+                payload.put("volMinus", "volMinus")
+                service.emit("pingVolMinus", payload)
+            }
+            btnMenu.setOnClickListener {
+                val payload = JSONObject()
+                payload.put("menu", "menu")
+                service.emit("pingMenu", payload)
+            }
+            btnHome.setOnClickListener {
+                val payload = JSONObject()
+                payload.put("home", "home")
+                service.emit("pingHome", payload)
+            }
+
+        }
     }
 }
--- example.js
+++ example.js
@@ -19,10 +19,30 @@ console.log('Hello, World!')
 LiquidCore.on( 'ping', (data) => {
     // When we get the ping from the host, respond with "Hello, World!"
     // and then exit.
-    console.log("from local," +data.hallo)
-    LiquidCore.emit( 'pong', { message: 'Hello, World from LiquidCore!' } )
-    process.exit(0)
+//    console.log("from local," +data.hallo)
+//    LiquidCore.emit( 'pong', { message: 'Hello, World from LiquidCore!' } )
+//    process.exit(0)
 })
 
+LiquidCore.on( 'pingPair', (data) => {
+    console.log("pair," +data.pair)
+
+})
+LiquidCore.on( 'pingConnect', (data) => {
+    console.log("connect," + data.connect)
+})
+LiquidCore.on( 'pingVolPlus', (data) => {
+   console.log("volPlus," + data.volPlus)
+})
+
+LiquidCore.on( 'pingVolMinus', (data) => {
+   console.log("volMinus," + data.volMinus)
+})
+LiquidCore.on( 'pingMenu', (data) => {
+   console.log("menu," + data.menu)
+})
+LiquidCore.on( 'pingHome', (data) => {
+   console.log("home," + data.home)
+})
 // Ok, we are all set up.  Let the host know we are ready to talk
 LiquidCore.emit( 'ready' )
